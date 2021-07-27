53edd5a758381d893e673384b7cce868caf67290
yanjing <jing.yan@enzenith.com>
2021-07-27 09:16:29 +0800

    项目初始化

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Index",
+    component: () => import("@/views/Index.vue"),
+    children: [
+      {
+        path: "/Home",
+        name: "Home",
+        component: () => import("@/views/Home.vue")
+      },
+      {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import("@/views/About.vue"),
+      },
+    ],
+    redirect: "/Home"
+  },
+
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
